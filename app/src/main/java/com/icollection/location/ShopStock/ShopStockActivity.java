d0fbb4b49542abcb62a2c77883fa60e815397431
Jackson <xyangui@163.com>
2021-03-04 10:38:57 +1100

    add shopstock activity and xml working

--- app/src/main/java/com/icollection/location/ShopStock/ShopStockActivity.java
+++ app/src/main/java/com/icollection/location/ShopStock/ShopStockActivity.java
@@ -28,14 +28,7 @@ import butterknife.OnClick;
 
 public class ShopStockActivity extends NetActivity {
 
-    @BindView(R.id.edit_barcode)
-    EditText editBarcode;
-    @BindView(R.id.textview_description)
-    TextView textDescription;
-    @BindView(R.id.recyclerview_shop_stock)
-    RecyclerView recyclerviewShopStock;
 
-    private ShopStockAdapter shopStockAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +36,6 @@ public class ShopStockActivity extends NetActivity {
         setContentView(R.layout.activity_shop_stock);
         ButterKnife.bind(this);
 
-        editBarcode.setTransformationMethod(new TransInformation());//全部转换成大写
-
-        editBarcode.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_UNSPECIFIED || actionId == EditorInfo.IME_ACTION_DONE) {
-                    //手持机扫描出barcode后
-//                    btnOK();
-                }
-                return false;
-            }
-        });
-
-        initAdapter();
     }
 
     @OnClick(R.id.image_view_back)
@@ -64,66 +43,7 @@ public class ShopStockActivity extends NetActivity {
         finish();
     }
 
-    /**
-     * 绑定适配器，显示系列其他产品位置列表
-     */
-    private void initAdapter() {
 
-        recyclerviewShopStock.setLayoutManager(new LinearLayoutManager(this));
-        recyclerviewShopStock.setItemAnimator(new DefaultItemAnimator());
 
-        shopStockAdapter = new ShopStockAdapter(new ArrayList<>(), this);
 
-        recyclerviewShopStock.setAdapter(shopStockAdapter);
-    }
-
-    // OK 按钮
-    @OnClick(R.id.btn_enter_json)
-    public void btnOK() {
-
-        textDescription.setText("");
-        shopStockAdapter.setNewData(null);
-
-        //String barcode = editBarcode.getText().toString().trim();
-        String barcode = "APIPHXSMHC103-0";
-        if (barcode.isEmpty()) {
-            ToastUtil.showShort(this, "Please enter barcode!");
-            return;
-        }
-        editBarcode.setText(barcode.toUpperCase());
-        editBarcode.requestFocus();
-
-        disposableAddWithProgress(
-                RemoteLocation
-                        .getInstance()
-                        .getShopStock(barcode),
-                strHtml -> {
-
-                    //jsonToShow(strHtml);
-                });
-    }
-//
-//    private void jsonToShow(String strHtml) {
-//
-//        if (strHtml.isEmpty() || strHtml.equals("null")) {
-//            ToastUtil.showShort(this, "No items were found!");
-//            return;
-//        }
-//
-//        Gson gson = new Gson();
-//        List<ShopStockBean> locationGets = gson.fromJson(strHtml, new TypeToken<List<LocationGet>>() {
-//        }.getType());
-//
-////        for (ShopStockBean locationGet : locationGets) {
-////
-////            String str = locationGet.getBcode().trim();
-////            String str2 = editBarcode.getText().toString().trim();
-////            if (str.equals(str2)) {
-////
-////                textDescription.setText(locationGet.getDescription());
-////            }
-////        }
-//
-//        shopStockAdapter.setNewData(locationGets);
-//    }
 }
